7c7194a7c2eee613b48310eb75741d99d9429cb4
camille-tech <delattre_camille@hotmail.fr>
2023-01-09 15:26:08 +0100

    permierfois

---
+++ src/main/java/com/example/demo_retourdevances/DemoRetourdevancesApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo_retourdevances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRetourdevancesApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(DemoRetourdevancesApplication.class, args);
+		System.out.println("coucou");
+	}
+
+}
